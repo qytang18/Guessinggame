36d609a8d24e7788c261ab7440798eb1d5f42444
qytang18 <anntang0318@gmail.com>
2018-11-24 22:51:00 -0800

    Add make and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Title of the Project: Guessinggame
+The time and date created: Sat Nov 24 22:45:56 PST 2018
+The number of lines of code contained in guessinggame.sh: 24
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,19 +1,24 @@
+function guess_number {
+	ans=$(ls -1 | wc -l)
+	guess=-1
+	while [[ $guess -ne $ans ]]
+	do 
+		echo "Please enter your guess:"
+		read guess
+		if [[ $guess -lt $ans ]]
+		then
+			echo "Too Low!"
+		elif [[ $guess -gt $ans ]]
+		then
+			echo "Too High!"
+		else 
+			echo "Congraduation! You are correct!"	
+		fi
+	done
+
+}
+
+
 echo "How many files are there in the current directory?"
-
-ans=$(ls -1 | wc -l)
-guess=-1
-
-while [[ $guess -ne $ans ]]
-do 
-	read -p "Please guess:" guess
-	if [[ $guess -lt $ans ]]
-	then
-		echo "Too Low!"
-	elif [[ $guess -gt $ans ]]
-	then
-		echo "Too High!"
-	else 
-		echo "Congraduation! You are correct!"	
-	fi
-
-done
+guess_number
+echo "End"
